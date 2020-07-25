4af9a81daa38a16bafdb9ad106a0406f51e5ece6
Zeljko Brckovic <zeljkobrckovic86@gmail.com>
2020-07-25 20:40:58 +0200

    Fix babel config for test and build

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,35 @@
-module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        useBuiltIns: 'entry',
-        targets: {
-          esmodules: true
-        },
-        corejs: 3,
-        modules: false
-      }
-    ],
-    '@babel/preset-typescript'
-  ]
+module.exports = api => {
+  const isTest = api.env('test')
+
+  if (isTest) {
+    return {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            useBuiltIns: 'entry',
+            corejs: 3,
+          }
+        ],
+        '@babel/preset-typescript'
+      ]
+    }
+  }
+
+  return {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          useBuiltIns: 'entry',
+          targets: {
+            esmodules: true
+          },
+          corejs: 3,
+          modules: false
+        }
+      ],
+      '@babel/preset-typescript'
+    ]
+  }
 }

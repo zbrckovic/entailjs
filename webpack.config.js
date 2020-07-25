9d203701400d164fd6c3dd853a475eb19dd4bcb3
Zeljko Brckovic <zeljkobrckovic86@gmail.com>
2020-07-25 17:34:27 +0200

    More work on webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+const SRC_DIR = path.resolve(__dirname, 'node_modules')
+const DIST_DIR = path.resolve(__dirname, 'dist')
+const NODE_MODULES_DIR = path.resolve(__dirname, 'node_modules')
+
 
 module.exports = (options = {}) => {
   const development = Boolean(options.development)
@@ -8,10 +14,13 @@ module.exports = (options = {}) => {
     entry: './src/index.ts',
     resolve: {
       extensions: ['.js', '.ts'],
-      modules: [path.resolve(__dirname, 'node_modules'), path.resolve(__dirname, 'src')]
+      modules: [NODE_MODULES_DIR, SRC_DIR]
     },
+    plugins: [
+      new CleanWebpackPlugin(),
+    ],
     output: {
-      path: path.resolve(__dirname, 'dist'),
+      path: DIST_DIR,
       filename: 'entail-core.js',
       library: 'entailCore',
       libraryTarget: 'umd'

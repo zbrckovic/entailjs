44e16a40ab6184d7dd75b5bbfcd610e8d3c26d3e
Zeljko Brckovic <zeljkobrckovic86@gmail.com>
2020-08-23 12:08:08 +0200

    Improve exports

--- src/deduction-structure/index.ts
+++ src/deduction-structure/index.ts
@@ -1,1 +1,2 @@
 export { Deduction } from './deduction'
+export { Rule } from './rule'
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export {
     Expression, ExpressionPointer, connectWithBinarySym
 } from './abstract-structures/expression'
 export { Category, Kind, Sym } from './abstract-structures/sym'
-export { Deduction } from './deduction-structure'
+export { Deduction, Rule } from './deduction-structure'
 export { FormulaParser, DeductionParser } from './parsers'
 export { ErrorName } from './error'
 export { DeductionInterface } from './deduction-interface'
@@ -12,3 +12,13 @@ export {
 export {
     isContingent, isContradiction, isFalsifiable, isSatisfiable, isTautology
 } from './propositional-logic'
+export {
+    existentialQuantifier,
+    conjunction,
+    disjunction,
+    equivalence,
+    implication,
+    negation,
+    primitiveSyms,
+    universalQuantifier,
+} from './primitive-syms'

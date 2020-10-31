b3bd9dd75d163b0ab1def7311937f936e7036f13
Zeljko Brckovic <zeljkobrckovic86@gmail.com>
2020-10-31 12:25:41 +0100

    Fixed validators.

--- src/parsers/validators.js
+++ src/parsers/validators.js
@@ -1,9 +1,9 @@
 // These validators are for the parts of `peg` grammar. They are here to be used in UI
 // implementations.
-const propositionalVariablePattern = /[a-z][0-9]?/
-const predicateVariablePattern = /[BCDF-SU-Z][0-9]?/
-const individualVariablePattern = /[a-z][0-9]?/
-const functionVariablePattern = /[a-z][0-9]?/
+const propositionalVariablePattern = /^[a-z][0-9]?$/
+const predicateVariablePattern = /^[BCDF-SU-Z][0-9]?$/
+const individualVariablePattern = /^[a-z][0-9]?$/
+const functionVariablePattern = /^[a-z][0-9]?$/
 
 export const isPropositionalVariable = text => propositionalVariablePattern.test(text)
 export const isPredicateVariable = text => predicateVariablePattern.test(text)
